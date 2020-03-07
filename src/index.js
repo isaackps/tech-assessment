81b0ee153f8bc52d572d75b316b2f52cd655d53f
isaackps <isaac.kps@gmail.com>
2020-03-07 09:30:53 +0800

    removed redux from the project template

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import store, { history } from './store';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <ConnectedRouter history={ history }>
-      <App />  
-    </ConnectedRouter>
-  </Provider>
+  <App />  
   , document.getElementById('root')
 );
 
--- src/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
-import home from './models/home';
-
-export default (history) => combineReducers({
-  router: connectRouter(history),
-  home
-});
